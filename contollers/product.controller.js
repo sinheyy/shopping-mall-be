b265706440f20d27c3dab0e9a03c85868e10d2c9
sinhey <sinheyyy@gmail.com>
2024-06-03 21:31:41 +0900

    add search

--- contollers/product.controller.js
+++ contollers/product.controller.js
@@ -16,8 +16,17 @@ productController.createProduct = async (req, res) => {
 
 productController.getProducts = async (req, res) => {
     try {
-        const products = await Product.find({});
-        res.status(200).json({ status: "success", products });
+        const { page, name } = req.query;
+        // if (name) {
+        //     const products = await Product.find({ name: { $regex: name, $options: "i" } });     // regex는 포함하는 거 보여주기 ,option i는 영어 대소문자 신경 안 씀
+        // }
+        // else {
+        //     const products = await Product.find({});
+        // }
+        const cond = name ? { name: { $regex: name, $options: "i" }}:{};
+        let query = Product.find(cond);
+        const productList = await query.exec();
+        res.status(200).json({ status: "success", data : productList });
     } catch (error) {
         res.status(400).json({ status: "fail", message: error.message });
     }
