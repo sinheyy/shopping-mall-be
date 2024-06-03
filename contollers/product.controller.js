d3a57f5218b334238611385f9dbb1b3475516836
sinhey <sinheyyy@gmail.com>
2024-06-03 16:56:55 +0900

    update get products

--- contollers/product.controller.js
+++ contollers/product.controller.js
@@ -14,4 +14,13 @@ productController.createProduct = async (req, res) => {
     }
 }
 
+productController.getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ status: "success", products });
+    } catch (error) {
+        res.status(400).json({ status: "fail", message: error.message });
+    }
+}
+
 module.exports = productController;
